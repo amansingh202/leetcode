660e8650b3c629b2f8a4f16a92836bb6e0acc8eb
Aman Kumar <60036234+amansingh202@users.noreply.github.com>
2024-03-12 21:20:44 -0500

    linkedList in DSA

---
+++ linkedList.py
@@ -0,0 +1,35 @@
+#linked list implementation in python
+
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr += str(itr.data) + '--->'
+            itr = itr.next
+
+        print(llstr)
+
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.insert_at_beginning(5)
+    ll.insert_at_beginning(89)
+    ll.print()
