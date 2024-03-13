131faa164b960bac67ca29e4bf7e6b6a9cbd7c12
Aman Kumar <60036234+amansingh202@users.noreply.github.com>
2024-03-12 20:21:06 -0500

    solution for 1

---
+++ arraysDS_exercise1.py
@@ -0,0 +1,21 @@
+'''
+Let us say your expense for every month are listed below,
+January - 2200
+February - 2350
+March - 2600
+April - 2130
+May - 2190
+Create a list to store these monthly expenses and using that find out,
+
+1. In Feb, how many dollars you spent extra compare to January?
+2. Find out your total expense in first quarter (first three months) of the year.
+3. Find out if you spent exactly 2000 dollars in any month
+4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+5. You returned an item that you bought in a month of April and
+got a refund of 200$. Make a correction to your monthly expense list
+based on this'''
+
+expenses = [2200, 2350, 2600, 2130, 2190]
+
+diff_feb_jan = expenses[1] - expenses[0]
+print(f"Solution for 1 is {diff_feb_jan}")
