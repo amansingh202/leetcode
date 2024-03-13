a2279551256854ef33474a180ba056cdcbdad2b1
Aman Kumar <60036234+amansingh202@users.noreply.github.com>
2024-03-13 14:41:03 -0500

    insert element at the beginning in a doubly linked list

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -25,8 +25,13 @@ class DoublyLinkedList:
         self.head = None
 
     def insert_at_beginning(self, data):
-        node = Node(data, self.head)
-        self.head = node
+        new_node = Node(data)
+        new_node.next = self.head
+
+        if self.head is not None:
+            self.head.prev = new_node
+
+        self.head = new_node
 
     def print(self):
 
