d641866a97d7074b649d6fb15e9224791269491d
Aman Kumar <60036234+amansingh202@users.noreply.github.com>
2024-06-24 17:04:30 -0500

    search by value

--- arraysDSA.py
+++ arraysDSA.py
@@ -3,3 +3,8 @@ stock_prices = [298,305,320,301,292]
 result = stock_prices[3]
 
 print(result)
+
+#searching by value
+for i in range(len(stock_prices)):
+    if stock_prices[i] == 301:
+        print(i)
