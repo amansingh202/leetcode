925f187e405834dca17a67761e7237419ed9a2a6
Aman Kumar <60036234+amansingh202@users.noreply.github.com>
2024-06-26 11:48:14 -0500

    insert at end, start, get length and create new linked list

---
+++ linkedlistDemo.py
@@ -0,0 +1,66 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr += str(itr.data) + '--->'
+            itr = itr.next
+
+        print(llstr)
+
+    def insert_at_end(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+
+        while itr:
+            count += 1
+            itr = itr.next
+
+        return count
+    
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.insert_at_beginning(5)
+    ll.insert_at_beginning(89)
+    ll.insert_at_end(45)
+    ll.insert_at_end(71)
+    # ll.insert_at_end(23)
+    ll.insert_values(["banana", "mango", "grapes", "orange"])
+    ll.print()
+    print(ll.get_length())
