af2d117d28a6f0ab774cc1e5e72f5f353d495c09
Aman Kumar <60036234+amansingh202@users.noreply.github.com>
2024-06-26 14:21:46 -0500

    hash mapping

---
+++ hashMap.py
@@ -0,0 +1,36 @@
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self,key):
+        h = 0
+        for char in key:
+            h += ord(char)
+
+        return h % self.MAX
+    
+    #add key value pair
+    def add(self,key,value):
+        h = self.get_hash(key)
+        self.arr[h] = value
+
+    def get(self,key):
+        h = self.get_hash(key)
+        return self.arr[h]
+    
+    def __setitem__(self, key, value):
+        h = self.get_hash(key)
+        self.arr[h] = value
+
+    def __getitem__(self,key):
+        h = self.get_hash(key)
+        return self.arr[h]
+    
+
+t = HashTable()
+t.add('march 6', 130)
+print(t.get_hash('march 9'))
+print(t.get('march 6'))
+t['march 6'] = 141
+print(t['march 6'])
